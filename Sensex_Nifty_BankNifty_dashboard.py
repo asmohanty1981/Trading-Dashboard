d7542f913ffb850489d5e2ee4059e3edd5aac8f9
asmohanty1981 <dba.mohantyasit@gmail.com>
2026-03-23 07:23:36 +0530

    Update Sensex_Nifty_BankNifty_dashboard.py

--- Sensex_Nifty_BankNifty_dashboard.py
+++ Sensex_Nifty_BankNifty_dashboard.py
@@ -13,7 +13,7 @@ import pytz
 
 # CONFIG
 API_KEY = "zirmcjssldz9okdc"
-ACCESS_TOKEN = "DaU1Ie6Lt977zpdNWUu8f12Jvw9VLJxN"
+ACCESS_TOKEN = "DvmZ6jITMnrbVk5JfbqVXdVhm7g3yBiz"
 
 kite = KiteConnect(api_key=API_KEY)
 kite.set_access_token(ACCESS_TOKEN)
